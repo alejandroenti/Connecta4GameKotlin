e76dad794050571fb44e0236a8cf97747fd1adea
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-10-20 19:16:21 +0200

    Inicialitzant activity amb el layout corresponent

---
+++ app/src/main/java/com/alejandrolopez/connecta4game/OpponentSelectionActivity.kt
@@ -0,0 +1,21 @@
+package com.alejandrolopez.connecta4game
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OpponentSelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_connection)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.connection)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
