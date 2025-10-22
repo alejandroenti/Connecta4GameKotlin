a390d70afb377262db410e6ff61efeb980a6125e
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-10-22 17:11:54 +0200

    Afegint classes i missatges per a tractar anb el WebSocket Server

--- app/src/main/java/com/alejandrolopez/connecta4game/classes/KeyValues.kt
+++ app/src/main/java/com/alejandrolopez/connecta4game/classes/KeyValues.kt
@@ -3,6 +3,12 @@ package com.alejandrolopez.connecta4game.classes
 enum class KeyValues (val value : String) {
     K_SET_PLAYER_NAME("setPlayerName"),
     K_TYPE("type"),
-    K_NAME("name")
-
+    K_NAME("name"),
+    K_CLIENT_NAME("clientName"),
+    K_VALUE("value"),
+    K_SERVER_DATA("serverData"),
+    K_COLOR("color"),
+    K_PLAY("play"),
+    K_CLIENT_LIST("clientsList"),
+    K_CLIENTS_LIST("clientsList")
 }
