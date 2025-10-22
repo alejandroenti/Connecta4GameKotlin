8a35364b716b2c802de13c98730509b68027713f
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2025-10-22 19:26:24 +0200

    Començant instància de Fragments per enviar les sol¨licituds de partida

--- app/src/main/java/com/alejandrolopez/connecta4game/OpponentSelectionActivity.kt
+++ app/src/main/java/com/alejandrolopez/connecta4game/OpponentSelectionActivity.kt
@@ -1,12 +1,17 @@
 package com.alejandrolopez.connecta4game
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.alejandrolopez.connecta4game.MainActivity.Companion.clients
+import com.alejandrolopez.connecta4game.fragments.SendInvitationFragment
 
 class OpponentSelectionActivity : AppCompatActivity() {
+
+    private lateinit var panel : LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,21 @@ class OpponentSelectionActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        panel = this.findViewById<LinearLayout>(R.id.sendInvitationPanel)
+    }
+
+    public fun createSendList() {
+        panel.removeAllViews()
+
+        for (client in clients) {
+            val fragment = SendInvitationFragment()
+            fragment.setName(client.name!!)
+
+            supportFragmentManager.beginTransaction()
+                .add(R.id.sendInvitationPanel, fragment)
+                .commit()
         }
     }
 }
--- app/src/main/java/com/alejandrolopez/connecta4game/fragments/SendInvitationFragment.kt
+++ app/src/main/java/com/alejandrolopez/connecta4game/fragments/SendInvitationFragment.kt
@@ -4,15 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.alejandrolopez.connecta4game.R
 
 class SendInvitationFragment : Fragment() {
+
+    private lateinit var userName : TextView
+    private lateinit var btn : ImageButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflas el layout XML de este fragment
         return inflater.inflate(R.layout.fragment_send_invitation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val textView = view.findViewById<TextView>(R.id.usernName)
+        val imageButton = view.findViewById<ImageButton>(R.id.sendInvitation)
+
+        imageButton.setOnClickListener {
+            // Send Invitation
+        }
+    }
+
+    public fun setName(name : String) {
+        userName.setText(name)
     }
 }
